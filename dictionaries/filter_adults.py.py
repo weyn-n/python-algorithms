5ea2938fde65e1340bd975d5870584c67534c629
weyn <kyryloulianenko@gmail.com>
2026-08-29 18:49:04 +0300

    filter_adults.py.py

---
+++ dictionaries/filter_adults.py.py
@@ -0,0 +1,16 @@
+users = {
+    "Alex": 20,
+    "John": 17,
+    "Maria": 22,
+    "Bob": 15,
+    "Kate": 19,
+    "Mike": 16
+}
+
+adults = {}
+
+for name, age in users.items():
+    if age >= 18:
+        adults[name] = age
+        
+print(adults)
